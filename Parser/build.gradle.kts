6eca6bd399bea2004a31d248ca3605ebe76882b5
Andrey Novikov <andresderes2002@gmail.com>
2023-03-01 11:49:32 +0300

    Modify parser

--- Parser/build.gradle.kts
+++ Parser/build.gradle.kts
@@ -14,6 +14,8 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 
     implementation("org.jsoup:jsoup:1.15.3")
+    implementation("org.seleniumhq.selenium:selenium-java:4.8.0")
+    implementation(("io.github.bonigarcia:webdrivermanager:5.3.2"))
 
     implementation(project(":Aggregator"))
 }
--- Parser/src/main/java/parser/citilink/ParserCitilink.java
+++ Parser/src/main/java/parser/citilink/ParserCitilink.java
@@ -1,24 +1,30 @@
 package parser.citilink;
 
-import app.models.Videocard;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
-import java.io.IOException;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ParserCitilink {
-    public void parser() throws IOException {
-        Document document = Jsoup.connect("https://www.citilink.ru/catalog/videokarty/")
-                .userAgent("Chrome/81.0.4044.138")
-                .referrer("http://www.google.com")
-                .get();
+    public void parser() {
+//        Document document = Jsoup.connect("https://www.citilink.ru/catalog/videokarty/")
+//                .userAgent("Chrome/81.0.4044.138")
+//                .referrer("http://www.google.com")
+//                .get();
+//
+//        Elements videocards = document.select("div.app-catalog-1o4umte.eevw8x70"); //div.e12wdlvo0.app-catalog-1bogmvw.e1loosed0      span.e4qu3681.e106ikdt0.app-catalog-5wkwgp.e1gjr6xo0
+//
+//        for (Element videocard:videocards) {
+//            System.out.println(videocard);
+//        }
 
-        Elements videocards = document.select("div.app-catalog-1o4umte.eevw8x70"); //div.e12wdlvo0.app-catalog-1bogmvw.e1loosed0      span.e4qu3681.e106ikdt0.app-catalog-5wkwgp.e1gjr6xo0
-
-        for (Element videocard:videocards) {
-            System.out.println(videocard);
-        }
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.addArguments("--headless");
+        options.addArguments("--disable-dev-shm-usage");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.citilink.ru/catalog/videokarty/");
+        driver.quit();
     }
 }
