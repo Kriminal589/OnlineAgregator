0c95443a40fd2fa2ba535b45792bb028bffc7855
Andrey <andresderes2002@gmail.com>
2023-03-01 11:56:19 +0300

    dfg

--- Parser/src/main/java/parser/citilink/ParserCitilink.java
+++ Parser/src/main/java/parser/citilink/ParserCitilink.java
@@ -1,24 +1,29 @@
 package parser.citilink;
 
-import app.models.Videocard;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//import org.jsoup.nodes.Element;
+//import org.jsoup.select.Elements;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 import java.io.IOException;
 
 public class ParserCitilink {
-    public void parser() throws IOException {
-        Document document = Jsoup.connect("https://www.citilink.ru/catalog/videokarty/")
-                .userAgent("Chrome/81.0.4044.138")
-                .referrer("http://www.google.com")
-                .get();
+    public void parser() {
+//        Document document = Jsoup.connect("https://www.citilink.ru/catalog/videokarty/")
+//                .userAgent("Chrome/81.0.4044.138")
+//                .referrer("http://www.google.com")
+//                .get();
+//
+//        Elements videocards = document.select("div.app-catalog-1o4umte.eevw8x70"); //div.e12wdlvo0.app-catalog-1bogmvw.e1loosed0      span.e4qu3681.e106ikdt0.app-catalog-5wkwgp.e1gjr6xo0
+//
+//        for (Element videocard:videocards) {
+//            System.out.println(videocard);
+//        }
 
-        Elements videocards = document.select("div.app-catalog-1o4umte.eevw8x70"); //div.e12wdlvo0.app-catalog-1bogmvw.e1loosed0      span.e4qu3681.e106ikdt0.app-catalog-5wkwgp.e1gjr6xo0
-
-        for (Element videocard:videocards) {
-            System.out.println(videocard);
-        }
+        WebDriver driver = new OperaDriver();
+        driver.get("https://www.citilink.ru/catalog/videokarty/");
     }
 }
